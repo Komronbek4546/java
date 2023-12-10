e68ac2424e149ddfd34eafbbe212bc79df4eab3c
Komronbek4546 <qkomron2@gmail.com>
2023-12-10 21:13:56 +0500

    day_4

---
+++ main.js
@@ -0,0 +1,160 @@
+//Boolen 1
+
+/*let a = prompt("sonni kiriting");
+
+if(a > 0){
+    console.log( a + "Musbat son");
+
+} */
+
+//Boolen 2
+
+/*let son= prompt("sonni kiriting"); 
+
+if (son % 2 === 0) {
+    console.log("Bu son juft son");
+} else {
+    console.log("Bu son toq son");
+} */
+
+// Boolen 3
+ /*let son = prompt("sonnig kiriting");
+
+if ( son % 2 === 0){
+    console.log("juft son");
+} else{
+    console.log("toq son");
+} */
+
+//boolen 4 
+ 
+/* let son1 = prompt("1-sonni kiriting");
+let son2 = prompt("2-sonni kiriting");
+
+if ( son1 > 2 && son2 <= 3){
+    console.log("true");
+
+} else{
+    console.log("falce");
+} */
+
+//Boolen 5 
+
+/* let son1 = prompt("1-sonni kiriting");
+let son2 = prompt("2-sonni kiriting");
+
+if ( son1 >= 0 && son2 < -2){
+    console.log("true");
+
+} else{
+    console.log("falce");
+} */
+
+//Boolen 6
+
+/*let son1 = prompt("1-sonni kiriting");
+let son2 = prompt("2-sonni kiriting");
+let son3 = prompt("3-sonni kiriting");
+
+if ( son1 <= son2 <= son3){
+    console.log("true");
+
+} else{
+        console.log("falce");
+}
+*/
+
+//Boolen 7 
+/*let son1 = prompt("sonni kiriting");
+let son2 = prompt("sonni kiriting");
+
+if (son1 % 2 === 0) {
+    console.log("Bu son juft son");
+} else if {
+    console.log("Bu son toq son");
+} else if(son2 % 2 === 0){
+
+    console.log("juft son");
+} else{
+    
+    console.log("toq son");
+
+}*/
+
+//Boolen 9 - 10
+/*let son1 = prompt("sonni kiriting");
+let son2 = prompt("sonni kiriting");
+
+if (son1 % 2 === 0) {
+    console.log("Bu son juft son");
+} else if {
+    console.log("Bu son toq son");
+} else if(son2 % 2 === 0){
+
+    console.log("juft son");
+} else{
+    
+    console.log("toq son");
+}*/
+
+
+
+//Boolen 12
+
+
+/*let son1 = prompt("1-sonni kiriting");
+let son2 = prompt("2-sonni kiriting");
+let son3 = prompt("3-sonni kiriting");
+
+if( son1>0 || son2>0 || son3>0){
+    console.log("musbat son")
+} else{
+    console.log("musbat son emas")
+}*/
+
+//Boolen 13
+
+/*var A = 5;  
+var B = -3; 
+var C = 8;  
+
+if (A > 0 || B > 0 || C > 0) {
+    console.log("A, B, C sonlarning hech bo'lmaganda bittasi musbat");
+} else {
+    console.log("A, B, C sonlarning hammasi manfiy");
+}*/
+
+
+//Boolen 14
+
+/*var A = 3;  
+var B = -2; 
+var C = 7;  
+
+// Sonlarni tekshirish
+if ((A > 0 && B > 0 && C <= 0) || (A > 0 && B <= 0 && C > 0) || (A <= 0 && B > 0 && C > 0)) {
+    console.log("A, B, C sonlardan faqat ikkitasi musbat son");
+} else {
+    console.log("Rostlikni tekshirish uchun boshqa variantni kiriting");
+}*/
+
+//Boolen 16
+
+/*var son = 48; 
+
+if (son >= 10 && son <= 99 && son % 2 === 0) {
+    console.log("Berilgan son ikki xonali juft son");
+} else {
+    console.log("Berilgan son ikki xonali juft son emas");
+}*/
+
+
+
+
+
+
+
+
+
+
+
